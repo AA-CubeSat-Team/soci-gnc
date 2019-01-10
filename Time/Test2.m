b2c518d2b6e9336922c74a8782063be91a7d5c97
IvanMachuca <45021870+IvanMachuca@users.noreply.github.com>
2019-01-10 14:00:38 -0800

    Tests the daysToYMD function

--- Time/Test2.m
+++ Time/Test2.m
@@ -1,16 +1,15 @@
 %Author- Ivan Machuca
-%Date Revised - 12/28/18
+%Date Revised - 1/10/18
+%This tests the daysToMonths function
+
 
 clear all; close all; clc; 
-UTC = 137803367; % Time in seconds since J2000. (Actual date of May 14,2004)
-%UTC = 599572800; %Time in seconds since J2000 . (Actual date of 12/31/18 )
+UTC = 137803367; %Time in seconds since J2000. (Actual date of May 14,2004 10:42)
+%UTC2 = 599572800;%Time in seconds since J2000 .(Actual date of Jan 1,2019 00:00)
 
-Days = UTC * (1/60)*(1/60)*(1/24);
-[YMD] = daysToYMD(Days);
-%This function gives 13.9464 days into the month. This means 13 full days
-%have passed and .9464 days can be converted to an hour/minute. On a
-%calendar, 13.9464 days puts the date on the 14th. So this function gives
-%the right amount of days and hours/minutes into the current month. 
+Days = UTC * (1/60)*(1/60)*(1/24); %Converting seconds into serial days. 
+[YMD] = daysToYMD(Days); %inputs serial days and outputs those serial days into
+%years months days. 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Below is just a test to test whether the YMD could return the actual date
@@ -25,7 +24,21 @@ D=floor(Days_into_month);
 H=floor(Hours);
 Min=floor(Minutes);
 
-[YMDHMins] = [YMD(:,1:2) D H Min s];
+[YMDHMins] = [YMD(:,1:2) D H Min s]%[Years Months Days Hours Minutes Seconds]
+%This timestamp reads as years since 2000, Month in the current year, calendar day
+%in the current month, hours of the current day, minutes of the current
+%hour, seconds of the current minute. 
 
 
-jd = juliandate([YMDHMins]); %calculated the julian date 
+
+
+
+
+
+
+
+
+
+
+
+
