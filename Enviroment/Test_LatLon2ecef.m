1fcf24773e2fa8d0e1b3604b4acbabe6d7e64f45
IvanMachuca <45021870+IvanMachuca@users.noreply.github.com>
2019-01-23 15:54:05 -0800

    LatLon2ecef and test

--- Enviroment/Test_LatLon2ecef.m
+++ Enviroment/Test_LatLon2ecef.m
@@ -5,8 +5,8 @@ clear all;close all;clc;
 %Testing the LatLon2ecef function
 %Checking Example 3-3 from the Textbook 
 he=5085.22;  %Altitude in Km
-l=34.352496; %Latitude in Km
-phi=46.4464; %Longitude in Km
+phi=34.352496; %Longitude in Km
+l=46.4464; %Latitide in Km
 r_ecef = LatLon2ecef(l,phi,he) 
 
 %checking to see if output is the same. %should get
