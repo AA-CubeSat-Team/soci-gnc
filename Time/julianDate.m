02929e4a2d0833748fd4eae0eaecd9a07b29569f
IvanMachuca <45021870+IvanMachuca@users.noreply.github.com>
2019-01-31 12:57:18 -0800

    Updated JulianDate function

--- Time/julianDate.m
+++ Time/julianDate.m
@@ -1,5 +1,11 @@
-% AUTHOR: Devan Tormey
-% DATE: 12/26/18
+%AUTHOR: Devan Tormey
+%DATE: 12/26/18
+
+%Date 1/31/19 
+%Updated as a division factor was left off. 
+%Author-Ivan Machuca
+
+
 function [JD] = julianDate(YMDHMS)
 %JULIANDATE Converts from YMDHMS to Julian Date
 %   Algorithm detailed on page 183 of Fundamentals of Astrodynamics and
@@ -13,9 +19,7 @@ h = YMDHMS(4);
 min = YMDHMS(5);
 s = YMDHMS(6);
 
-JD = 367*yr - floor((7*(yr + floor((mo + 9)/(12))))/4) + floor((175*mo)/9)...
-    + d + 1721013.5 + (((s/60) + min) + h)/24;
+JD = 367*yr - floor((7*(yr + floor((mo + 9)/(12))))/4) + floor((275*mo)/9)...
+    + d + 1721013.5 + (((s/60) + min)/60 + h)/24;
 
-
-end
 
