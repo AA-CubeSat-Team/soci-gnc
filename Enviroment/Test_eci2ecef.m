8f15ce125ae8975cf00faee218daa09fd95ac9c3
IvanMachuca <45021870+IvanMachuca@users.noreply.github.com>
2019-01-31 13:38:31 -0800

    Updated function

--- Enviroment/Test_eci2ecef.m
+++ Enviroment/Test_eci2ecef.m
@@ -6,8 +6,10 @@ clear all;close all; clc;
 %Example 3-15 in the textbook 
 UTC = 366*24*3600 + 365*24*3600*3 + (31+29+31+4)*24*3600 + ...
     7*3600+51*60+28.386009;% Time in seconds since J2000 (Actual date 4/16/2004 7:51:28.386009) PM) UTC
-TT = UTC + 32 + 32.164; %Time in seconds since J2000 in TT
-Days = UTC * (1/60)*(1/60)*(1/24); %Converting seconds into serial days. 
+
+TT = UTC + 32 + 32.184; %Time in seconds since J2000 in TT
+
+Days = TT * (1/60)*(1/60)*(1/24); %Converting seconds into serial days. 
 
 [YMD] = daysToYMD(Days); %inputs serial days and outputs those serial days into
 Days_into_month = YMD(:,3);
@@ -19,13 +21,22 @@ H=floor(Hours); %Hours
 Min=floor(Minutes); %seconds
 
 %Getting the YMDHMins
-[YMDHMins] = [YMD(:,1:2) D H Min s];
+[YMDHMins] = [YMD(:,1) YMD(:,2) D H Min s];
+Jd_tt = julianDate(YMDHMins);%Julian Date of TT
 
-Jd_tt = juliandate(YMDHMins); %Julian Date of TT
+
 [ttt] = julianCentury(Jd_tt); %Julian Centuries of TT
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Testing precession function
 [prec,psai,wa,ea,xa]=precess_funct(ttt);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
+
 
 
 
