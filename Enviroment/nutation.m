b10fd7e5c73d3d23d766eb4cb8fa5957b77ce2fe
willik50 <45180338+willik50@users.noreply.github.com>
2019-01-31 14:44:44 -0800

    Update nutation.m

--- Enviroment/nutation.m
+++ Enviroment/nutation.m
@@ -2,10 +2,9 @@
 %Date 1/24/2019
 
 
-function [deltapsi, trueeps, meaneps,nut] = nutation(ttt)
-
+function [deltapsi, trueeps, meaneps,nut,mC,mO,umc,dO,omega] = nutation(ttt)
+        %OUTPUT ANGLES IN RADIANTS
         deg2rad = pi/180.0; %deg to rad
-        [iar80,rar80] = iau80in;  % coeff in deg, for equ. 3-83 of Vallado
 
         ttt2= ttt*ttt; %ttt is julian century in tt time frame
         ttt3= ttt2*ttt;
@@ -13,15 +12,33 @@ function [deltapsi, trueeps, meaneps,nut] = nutation(ttt)
         %mean obliquity of the ecliptic in rads
         meaneps = -46.8150 *ttt - 0.00059 *ttt2 + 0.001813 *ttt3 + 84381.448;
         meaneps = mod( meaneps/3600.0, 360.0 )* deg2rad; %remainder after division converted to rad
-
+       
+        % ---- iau 1980 theory
+        load('nut80.dat')
+        iar80 = nut80(:,1:5); %api from equ 3-83 of vallado
+        rar80 = nut80(:,6:9); %Ai,Bi,Ci,Di
+        
+        mC = ((((0.064) * ttt + 31.310) * ttt + 1717915922.6330) * ttt) / 3600.0 + 134.96298139;
+        mO = ((((-0.012) * ttt - 0.577) * ttt + 129596581.2240) * ttt) / 3600.0 + 357.52772333;
+        umc = ((((0.011) * ttt - 13.257) * ttt + 1739527263.1370) * ttt) / 3600.0 + 93.27191028;
+        dO = ((((0.019) * ttt - 6.891) * ttt + 1602961601.3280) * ttt) / 3600.0 + 297.85036306;
+        omega = ((((0.008) * ttt + 7.455) * ttt - 6962890.5390) * ttt) / 3600.0 + 125.04452222;
+            
+        % ---- ANGLES of nutation from equ 3-82 of vallado
+        mC    = rem( mC,360.0  )     * deg2rad; % rad
+        mO   = rem( mO,360.0  )    * deg2rad;
+        umc    = rem( umc,360.0  )     * deg2rad;
+        dO    = rem( dO,360.0  )     * deg2rad;
+        omega= rem( omega,360.0  ) * deg2rad;
+        
         %find nutation in longitude and nutation in obliquity
         deltapsi= 0.0; %nutation angle in rad set initially to zero
         deltaeps= 0.0; %change in obliquity in rad
         for i= 106:-1: 1 %see page 226 equ. 3-83 of Vallado
-            tempval= iar80(i,1)*l + iar80(i,2)*l1 + iar80(i,3)*f + ...
-                     iar80(i,4)*d + iar80(i,5)*omega;
-            deltapsi= deltapsi + (rar80(i,1)+rar80(i,2)*ttt) * sin( tempval );
-            deltaeps= deltaeps + (rar80(i,3)+rar80(i,4)*ttt) * cos( tempval );
+            tempval= iar80(i,1)*mC + iar80(i,2)*mO + iar80(i,3)*umc + ...
+                     iar80(i,4)*dO + iar80(i,5)*omega;
+            deltapsi= deltapsi + (rar80(i,1)+rar80(i,2)*ttt) * sin( tempval);
+            deltaeps= deltaeps + (rar80(i,3)+rar80(i,4)*ttt) * cos( tempval);
         end
 
         %final nutation parameters, see page 226 equ. 3-85of Vallado
