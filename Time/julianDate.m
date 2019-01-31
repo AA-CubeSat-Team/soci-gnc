ab661b41d89fdfac2c8c1da76f317c6a1148c919
IvanMachuca <45021870+IvanMachuca@users.noreply.github.com>
2019-01-31 13:17:27 -0800

    Updated julianDate

--- Time/julianDate.m
+++ Time/julianDate.m
@@ -2,7 +2,8 @@
 %DATE: 12/26/18
 
 %Date 1/31/19 
-%Updated as a division factor was left off. 
+%Updated as a division factor was left off.Additionally, you needed to add 2000 to the...
+%year to make it work. Note year must be 2000 and above to work. 
 %Author-Ivan Machuca
 
 
@@ -12,14 +13,14 @@ function [JD] = julianDate(YMDHMS)
 %   Applications
 
 %extracting Variables from vector
-yr = YMDHMS(1);
+yr = YMDHMS(1); %year since 2000. 
 mo = YMDHMS(2);
 d = YMDHMS(3);
 h = YMDHMS(4);
 min = YMDHMS(5);
 s = YMDHMS(6);
 
-JD = 367*yr - floor((7*(yr + floor((mo + 9)/(12))))/4) + floor((275*mo)/9)...
+JD = 367*(yr+2000) - floor((7*((yr+2000) + floor((mo + 9)/(12))))/4) + floor((275*mo)/9)...
     + d + 1721013.5 + (((s/60) + min)/60 + h)/24;
 
 
