f8d4a5818d3efca233f471ea9de5ceca573a9da1
IvanMachuca <45021870+IvanMachuca@users.noreply.github.com>
2019-02-03 12:20:21 -0800

    Update ECI2ECEF_TEST.m

--- Enviroment/ECI2ECEF_TEST.m
+++ Enviroment/ECI2ECEF_TEST.m
@@ -9,7 +9,7 @@ function [recef,vecef,aecef]=ECI2ECEF_TEST(ttt,reci,veci,aeci)
 %Calculate the transformation matrix that calculates the effect of
 %precession %output from these are psia,wa,ea,xa,zeta,theta,z
 
-[prec,psia,wa,ea,xa,zeta,theta,z] = precess_funct(ttt);
+[prec,zeta,theta,z] = precess_funct(ttt);
 
 %*********************** Part 2 Nutation ***************************
 %Calculate the transformation matrix that calculates the effect of
