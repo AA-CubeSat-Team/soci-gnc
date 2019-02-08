c298f1546f6bde803b01c77ea2b9734906ca8dcc
willik50 <45180338+willik50@users.noreply.github.com>
2019-02-07 22:55:07 -0800

    Update rotationtoquaternion.m
    rotation matrix was set to start at 0 instead of 1

--- Enviroment/rotationtoquaternion.m
+++ Enviroment/rotationtoquaternion.m
@@ -5,7 +5,7 @@
 
 function [q0,q1,q2,q3]=rotationtoquaternion(R)
     q0=sqrt(1+R(1,1)+R(2,2)+R(3,3))/2; %real component
-    q1=(R(2,1)-R(1,2))/(4*q0);%vector component x on imaginary axis
-    q2=(R(0,2)-R(2,0))/(4*q0);%vector component y on imaginary axis
-    q3=(R(1,0)-R(0,1))/(4*q0);%vector component y on imaginary axis
+    q1=(R(3,2)-R(2,3))/(4*q0);%vector component x on imaginary axis
+    q2=(R(1,3)-R(3,1))/(4*q0);%vector component y on imaginary axis
+    q3=(R(2,1)-R(1,2))/(4*q0);%vector component y on imaginary axis
 end
