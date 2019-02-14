146ca6e2f7da2ec35891d92ce98704efce7f735a
willik50 <45180338+willik50@users.noreply.github.com>
2019-02-13 16:02:14 -0800

    Update nutation.m
    Last edit by Kate Williams and Ivan Machuca 2/13/2019
    -Entered table in directly, changed angle equations, double checked unit conversions

--- Enviroment/nutation.m
+++ Enviroment/nutation.m
@@ -1,7 +1,12 @@
 %Author Kate Williams
 %Date 1/24/2019
-function [deltapsi, trueeps, meaneps,nut,mC,mO,umc,dO,omega] = nutation(ttt)
-        %OUTPUT ANGLES IN RADIANTS
+
+%Last edit by Kate Williams and Ivan Machuca 2/13/2019
+%Entered table in directly, changed angle equations, double checked unit
+%conversions
+function [deltapsi, trueeps, meaneps,nut,M_moon,M_sun,U_moon,D_sun,O_moon] = nutation(ttt)
+
+        %Constants
         deg2rad = pi/180.0; %deg to rad
         rad2deg = 1/deg2rad;
         asec2rad = 1/3600*deg2rad;
@@ -12,34 +17,76 @@ function [deltapsi, trueeps, meaneps,nut,mC,mO,umc,dO,omega] = nutation(ttt)
         meaneps = -46.8150 *ttt - 0.00059 *ttt2 + 0.001813 *ttt3 + 84381.448;
         meaneps = mod( meaneps/3600.0, 360.0 )* deg2rad; %remainder after division converted to rad
        
-        % ---- iau 1980 theory
-        nut80=load('nut80.dat');
-        iar80 = nut80(:,1:5); %api from equ 3-83 of vallado
-        rar80 = nut80(:,6:9); %Ai,Bi,Ci,Di
+                % ---- iau 1980 theory
+        nut80= [ ...
+        1   0   0   0   0   1   -171996 -174.2  92025   8.9
+        9   0   0   2   -2  2   -13187  -1.6    5736    -3.1
+        31  0   0   2   0   2   -2274   -0.2    977     -0.5
+        2   0   0   0   0   2   2062    0.2     -895    0.5
+        10  0   1   0  	0   0   1426    -3.4    54      -0.1
+        32  1   0   0   0   0   712     0.1     -7      0.0
+        11  0   1   2   -2  2   -517    1.2     224     -0.6
+        33  0   0   2   0   1   -386    -0.4    200     0.0
+        34  1   0   2   0   2   -301    0.0     129     -0.1
+        12  0   -1  2   -2  2   217     -0.5    -95     0.3
+        35  1   0   0   -2  0   -158    0.0     -1      0.0
+        13  0   0   2   -2  1   129     0.1     -70     0.0 
+        36  -1  0   2   0   2   123     0.0     -53     0.0 
+        38  1   0   0   0   1   63      0.1     -33     0.0
+        37  0   0   0   2   0   63      0.0     -2      0.0
+        40  -1  0   2   2   2   -59     0.0     26      0.0
+        39  -1  0   0   0   1   -58     -0.1    32      0.0
+        41  1   0   2   0   1   -51     0.0     27      0.0
+        14  2   0   0   -2  0   48      0.0     1       0.0
+        3   -2  0   2   0   1   46      0.0     -24     0.0
+        42  0   0   2   2   2   -38     0.0     16      0.0
+        45  2   0   2   0   2   -31     0.0     13      0.0
+        43  2   0   0   0   0   29      0.0     -1      0.0
+        44  1   0   2   -2  2   29      0.0     -12     0.0
+        46  0   0   2   0   0   26      0.0     -1      0.0
+        15  0   0   2   -2  0   -22     0.0     0       0.0
+        47  -1  0   2   0   1   21      0.0     -10     0.0
+        16  0   2   0   0   0   17      -0.1    0       0.0
+        18  0   2   2   -2  2   -16     0.1     7       0.0
+        48  -1  0   0   2   1   16      0.0     -8      0.0 ...
+        ];
+    
+        iar80 = nut80(:,2:6); %api from equ 3-83 of vallado
+        rar80 = nut80(:,7:10); %Ai,Bi,Ci,Di
         
-        
-        mC = ((((0.064) * ttt + 31.310) * ttt + 1717915922.6330) * ttt) / 3600.0 + 134.96298139;
-        mO = ((((-0.012) * ttt - 0.577) * ttt + 129596581.2240) * ttt) / 3600.0 + 357.52772333;
-        umc = ((((0.011) * ttt - 13.257) * ttt + 1739527263.1370) * ttt) / 3600.0 + 93.27191028;
-        dO = ((((0.019) * ttt - 6.891) * ttt + 1602961601.3280) * ttt) / 3600.0 + 297.85036306;
-        omega = ((((0.008) * ttt + 7.455) * ttt - 6962890.5390) * ttt) / 3600.0 + 125.04452222;
+        M_moon	=   deg2rad * ( 134.96298139+(1325 * 360 + 198.8673981) * ttt ...
+                      	+ 0.0086972 * ttt2 + 1.78e-5 * ttt3);
+               
+        M_sun	=   deg2rad * ( 357.52772333 + (99 * 360 + 359.0503400) * ttt ...
+                        - 0.0001603 * ttt2 - 3.3e-6 * ttt3);
+
+        U_moon	=   deg2rad * ( 93.27191028 + (1342 * 360 + 82.0175381) * ttt ...
+                        - 0.0036825 * ttt2 + 3.1e-6 * ttt3);
+
+        D_sun	=   deg2rad * ( 297.85036306 + (1236 * 360 + 307.1114800) * ttt ...
+                        - 0.0019142 * ttt^2 + 5.3e-6 *ttt^3);
+
+        O_moon	=   deg2rad * ( 125.04452222 - (5 * 360 + 134.1362608) * ttt ...
+                        + 0.0020708 * ttt^2 + 2.2e-6 * ttt^3);       
             
         % ---- ANGLES of nutation from equ 3-82 of vallado 
-        mC    = rem( mC,360.0  )     * deg2rad; % rad %moon
-        mO   = rem( mO,360.0  )    * deg2rad;%sun
-        umc    = rem( umc,360.0  )     * deg2rad;%moon
-        dO    = rem( dO,360.0  )     * deg2rad;%sun
-        omega= rem( omega,360.0  ) * deg2rad;%moon
+        M_moon    = rem( M_moon,360.0  )     * deg2rad; % rad %moon
+        M_sun   = rem( M_sun,360.0  )    * deg2rad;%sun
+        U_moon    = rem( U_moon,360.0  )     * deg2rad;%moon
+        D_sun    = rem( D_sun,360.0  )     * deg2rad;%sun
+        O_moon= rem( O_moon,360.0  ) * deg2rad;%moon
         
         %find nutation in longitude and nutation in obliquity
         deltapsi= 0.0; %nutation angle in rad set initially to zero
         deltaeps= 0.0; %change in obliquity in rad
         for i= 106:-1: 1 %see page 226 equ. 3-83 of Vallado
-            tempval= iar80(i,1)*mC + iar80(i,2)*mO + iar80(i,3)*umc + ...
-                     iar80(i,4)*dO + iar80(i,5)*omega;
+            tempval= iar80(i,1)*M_moon + iar80(i,2)*M_sun + iar80(i,3)*U_moon + ...
+                     iar80(i,4)*D_sun + iar80(i,5)*O_moon;
             deltapsi= deltapsi + ((rar80(i,1)+rar80(i,2)*ttt))*0.0001 * sin(tempval);
             deltaeps= deltaeps + ((rar80(i,3)+rar80(i,4)*ttt))*0.0001 * cos(tempval);
         end
+        
+        %testing
         deltapsi*asec2rad*rad2deg;
         deltaeps*asec2rad*rad2deg;
         
