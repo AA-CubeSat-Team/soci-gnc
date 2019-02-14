9bf514ad2a9b11f40a4d878532992d23e46973f6
IvanMachuca <45021870+IvanMachuca@users.noreply.github.com>
2019-02-13 19:12:34 -0800

    updated inputs

--- Enviroment/ECI2ECEF2SEZ_UnitTesting.m
+++ Enviroment/ECI2ECEF2SEZ_UnitTesting.m
@@ -9,15 +9,18 @@ clear all; close all; clc;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Inputs
-reci = [-1033.479383;7901.2954;6380.3565958]; %km 
+reci = [5102.508958;6123.011401;6378.136928]; %km 
 ttt = 0.0426236319; %Taken from example 3-15 Julian Centuries in TT
 jdut1= 0.0426236114109;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Transforming from ECI2ECEF
-[recef]=eci2ecef(ttt,jdut1,reci)
+[recef]=eci2ecef(ttt,jdut1,reci);
+recef_actual = [-1033.479383,7901.2952754,6380.3565958]';
+pos = [recef recef_actual];
 
-%recef should be 5102.508,6123.001,6378.136928
+
+%recef should be-1033.479383,7901.2952754,6380.3565958
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Transforming from ECEF_to_LatLon
 [phi,lamda,he] = ECEF_to_LatLon(recef);
