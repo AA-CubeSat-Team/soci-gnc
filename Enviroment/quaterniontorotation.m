b931794c7b969558ba1f0979c293cfd1256563e9
willik50 <45180338+willik50@users.noreply.github.com>
2019-03-10 16:02:42 -0700

    Update quaterniontorotation.m

--- Enviroment/quaterniontorotation.m
+++ Enviroment/quaterniontorotation.m
@@ -8,8 +8,8 @@
 function [R]=quaterniontorotation(q0,q1,q2,q3)
     R=zeros(3);
     R(1,1)=1-2*q2^2-2*q3^2; %column 1 row 1 of rotation matrix
-    R(2,1)=2*q1*q2+2*q3*q0; %column 1 row 2...
-    R(3,1)=2*q1*q3-2*q2*q0;
+    R(1,2)=2*q1*q2+2*q3*q0; %column 1 row 2...
+    R(1,3)=2*q1*q3-2*q2*q0;
     R(2,1)=2*q1*q2-2*q3*q0;
     R(2,2)=1-2*q1^2-2*q3^2;
     R(2,3)=2*q2*q3+2*q1*q0;
