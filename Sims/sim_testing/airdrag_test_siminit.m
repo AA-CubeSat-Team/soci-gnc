1d507811bd03994ae2c2f6e951958fa0dfccdfd6
willik50 <45180338+willik50@users.noreply.github.com>
2019-04-03 12:30:48 -0700

    Update airdrag_test_siminit.m

--- Sims/sim_testing/airdrag_test_siminit.m
+++ Sims/sim_testing/airdrag_test_siminit.m
@@ -1,10 +1,16 @@
 %sim 
+%Author Kate Williams
 clc; clear;
+
+addpath(genpath(pwd))
+addpath(genpath('../../Libraries'))
+addpath(genpath('../../Libraries/'))
+
 
 G=6.673*10^(-11); %N*m^2/kg^2
 R= 6.38*10^6+500*10^3;%m, assume orbit of 500km
 M=5.98*10^24 ;%kg, mass of earth
-vin = sqrt(G*M/R); %m/s
+vin =sqrt(G*M/R);%m/s
 
 %velocity relative to the atmosphere
 
@@ -17,9 +23,19 @@ wy=0; %omega of earth in y direction
 %vel in m/s, vrel in m/s
 vrel = [vin 0 0];%assume for now velocity in only one direction for testing
 UTC= 10^6;
-muh= 3.986004418e14;%m^3/s^2,assumed constant
-u_h= muh/(500e3); %muh/(altitude of cubesat in meters),assumed constant
 
-sim('atmoDrag_lib.slx');
+muh= 3.986004418e14;%m^3/s^2,assumed constant
+%u_h= muh/(500e3); %muh/(altitude of cubesat in meters),assumed constant
+
+lon = 60;    % [deg]
+lat  = -70;  % [deg]
+height = 500;  % [km]
+u_h=[lon;lat;height];
+
+sim('atmoDrag_testModel.slx');
+
+torque=AtmoTorque_Mn;
+
+
 
 
