c802879a1fd3a4d0708fd6cd35e319567f202d26
dtormey <dtormey@usc.edu>
2019-04-09 14:30:28 -0700

    Time library ready for unit test

--- Test/Time_Coordinates/Time/time_test_init.m
+++ Test/Time_Coordinates/Time/time_test_init.m
@@ -1,6 +1,23 @@
-utc_test = 137803367;
+clc
+
+utc_test = 137803367
 
 sim('time_test.slx')
 
-simout.data
+tt_out = ymdhms_tt.data
 
+% tol = 1;
+
+% if abs(tt_out - tt_answer) < tol
+%     pass = true;
+
+ut1_out = ymdhms_ut1.data
+
+
+
+
+% if pass
+%     fprintf('Time test Passed')
+% else 
+%     fprintf('Time test Failed')
+% end
