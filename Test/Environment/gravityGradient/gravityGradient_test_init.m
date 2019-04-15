1c9ede3e4928bf12247e0eef165ffad0218b448f
Ivan Machuca <IvanMachuca@Ivan-Machucas-MacBook-Pro.local>
2019-04-14 22:58:10 -0700

    Updated Test Files

--- Test/Environment/gravityGradient/gravityGradient_test_init.m
+++ Test/Environment/gravityGradient/gravityGradient_test_init.m
@@ -1,24 +1,13 @@
-clc , 
+%Author I.Machuca
+%SOC-I GNC
+%Gravity gradient Torques test
+%The function of this test is to calculate the torques due to gravity
+%gradients as these will be disturbances to our system. 
 
-%Gravity gradient test
-reci = [5102.508958;6123.011401;6378.136928]; %km initial position
-quat = [0.371;0.649;0.654;0.106];%quaternion
-w = [1 1 1]';
-v = [1 1 1]';
+clc, 
 
-state = [reci;v;quat;w];
-
-
+reci = [5102.508958;6123.011401;6378.136928]; %km position from example 3.13 is Vallado
+v = [1 1 1]'; %dummy variable for now for velocity
+state = [qd;reci;w0;v]; %Example state vector
 sim('gravityGradient_test.slx')
 
-%Below is an attempted to capture the pitch dynamics of the spacecraft with
-%the calculated gravity gradient torques from the sim. 
-%Assuming a period of 90 mins for the spacecraft 
-T =90;
-n = 2*pi/(T*60);
-a =sqrt(-3*n^2/J(2,2)*(J(3,3)-J(1,1)));
-theta_0 =5*(pi/180);
-theta_0dot = -0.1*(pi/180);
-t = linspace(0,5);
-pitch = Torque_GG(2)/(J(2,2)*a^2) + (theta_0 - Torque_GG(2)/(J(2,2)*a^2))*cos(a*t) + theta_0dot/a^2 *sin(a*t);
-plot(t,pitch)
