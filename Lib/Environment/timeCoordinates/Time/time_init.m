81cf29918b816c467271d7c97a6d0a7b2dc5a7cc
dtormey <dtormey@usc.edu>
2019-04-17 16:05:15 -0700

    tested the clock with the time library need to find utc epoch in J2000 (its the 1970 epoch right now)

--- Lib/Environment/timeCoordinates/Time/time_init.m
+++ Lib/Environment/timeCoordinates/Time/time_init.m
@@ -16,5 +16,11 @@ time.DUT1 = -0.017795; %UTC -> UT1 Offset
 time.TAI_offset = 37; %UTC -> TAI offset
 time.DTT_TAI = 32.184; %TAI -> TT offset
 
+
+%  Mission start time (arbitrarily apr 17 2019 3:14PM)
+time.epoch_utc_s = 1555539201;
+% Time resolution
+time.res = 1;
+
 simParams.time = time;
 clear time
---
+++ Test/Environment/timeCoordinates/Time/clock_test_init.m
@@ -0,0 +1,7 @@
+clc
+
+sim('clock_test.slx')
+
+time_utc.Data
+
+ymdhms = time_ymdhms_utc.Data
