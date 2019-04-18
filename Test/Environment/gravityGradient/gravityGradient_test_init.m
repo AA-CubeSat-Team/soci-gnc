409e9ca20f977b1fabefc658c1b10d728e3627d1
IvanMachuca <45021870+IvanMachuca@users.noreply.github.com>
2019-04-17 20:24:49 -0700

    Updated Files with acceleration

--- Test/Environment/gravityGradient/gravityGradient_test_init.m
+++ Test/Environment/gravityGradient/gravityGradient_test_init.m
@@ -5,8 +5,11 @@
 %gradients as these will be disturbances to our system. 
 
 clc, 
-
-reci = [5102.508958;6123.011401;6378.136928]; %km position from example 3.13 is Vallado
+jdut1 = 2453101.82740678; %Julian Dated in UT1
+ttt = 0.0426236319; %Julian Centuries in TT
+%reci = [5102.508958;6123.011401;6378.136928]; %km position from example 3.13 is Vallado
+reci=[0.1;0.1;6371];
+time =[ttt;jdut1];
 v = [1 1 1]'; %dummy variable for now for velocity
 state = [qd;reci;w0;v]; %Example state vector
 sim('gravityGradient_test.slx')
