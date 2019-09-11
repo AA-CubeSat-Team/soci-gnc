80b973fd886549a90c450e4a045d0ec61ae79ee6
Taylor Patrick Reynolds <tpr6@uw.edu>
2019-09-11 16:46:34 -0700

    updated atmodrag, magField, clock_lib, and created an environment library. Removed several constants from these libs and put them in the appropriate init file

--- Lib/Dynamics/scParams_init.m
+++ Lib/Dynamics/scParams_init.m
@@ -1,32 +1,30 @@
-
 scParams = struct;
 
-
 % Dimension of cubesat
-scParams.z_len = .30; %cm
-scParams.x_len = .10; %cm
-scParams.y_len = .10; %cm
-scParams.m_3u = 4; %kg
+scParams.z_len  = 0.30; %cm
+scParams.x_len  = 0.10; %cm
+scParams.y_len  = 0.10; %cm
+scParams.m_3u   = 4.00; %kg
 
-
+% Body normals in the body frame
+scParams.xB_dir = [1,0,0];
+scParams.yB_dir = [0,1,0];
+scParams.zB_dir = [0,0,1];
 
 % Inertia Tensor
-scParams.J = [(1/12)*scParams.m_3u*(scParams.z_len^2 + scParams.x_len^2),0,0;
-    0,(1/12)*scParams.m_3u*(scParams.x_len^2 + scParams.z_len^2), 0;
-     0,0,(1/12)*scParams.m_3u*(scParams.y_len^2 + scParams.x_len^2)];
+scParams.J = [...
+     (1/12)*scParams.m_3u*(scParams.z_len^2 + scParams.x_len^2),0,0;
+     0,(1/12)*scParams.m_3u*(scParams.x_len^2 + scParams.z_len^2),0;
+     0,0,(1/12)*scParams.m_3u*(scParams.y_len^2 + scParams.x_len^2) ];
  
-% Point of pressure for S/C
+% Point of pressure for S/C and estimated drag coefficent per face
 scParams.point_of_pressure = [0.01; 0.02; 0.03];
+scParams.CD = 2.2; % standard for 3U cubesat
 
 % Area of the faces of the S/C
-scParams.Ax = .003;
-scParams.Ay = .003;
-scParams.Az = .001;
-
-scParams.Re_km = 6378137.0;
-
-
-
+scParams.Ax = scParams.x_len * scParams.z_len;
+scParams.Ay = scParams.y_len * scParams.z_len;
+scParams.Az = scParams.x_len * scParams.y_len;
 
 simParams.scParams = scParams;
 clear scParams
