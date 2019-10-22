a23e930a4eb1392229ed3560451b2866c3e0071d
Taylor Patrick Reynolds <tpr6@uw.edu>
2019-10-21 17:47:15 -0700

    updated the reaction wheel assembly model and the torque to rpm conversion

--- Lib/Dynamics/intitialConditions_init.m
+++ Lib/Dynamics/intitialConditions_init.m
@@ -22,33 +22,7 @@ v0_eci_kmps             = fswParams.constants.convert.M2KM * v0_eci_mps;
 initialConditions.r0    = r0_eci_km;
 initialConditions.v0    = v0_eci_kmps;
 
-% % ~~~~~~~~~~~~~~~~~~~~~
-% % Option 1 equitorial Orbit
-% % ~~~~~~~~~~~~~~~~~~~~~
-% % Calculate an initial r and v
-% r = [500+fswParams.constants.global.r_earth_km;0;0];
-% v = [0;sqrt(fswParams.constants.global.mu_earth_km3s2/norm(r));0];
-% initialConditions.r0 = r;
-% initialConditions.v0 = v;
-
-% % ~~~~~~~~~~~~~~~~~~~~~
-% % Option 2 ISS Orbit (From NASA Website)
-% % ~~~~~~~~~~~~~~~~~~~~~
-% r = [-2447628.5;4868517.89;-4051581.44]*10^(-3);
-% v = [-6580.108569;-49.846368;3920.219467]*10^(-3);
-% initialConditions.r0 = r;
-% initialConditions.v0 = v;
-
-% % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% % Option 3 ISS Orbit From Initial TLE
-% % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% t0 = simParams.time.epoch_utc_s; %Initial Time (From time_init.m)
-% % Orbital Elements
-% r = [-2447628.5;4868517.89;-4051581.44]*10^(-3);
-% v = [-6580.108569;-49.846368;3920.219467]*10^(-3);
-% initialConditions.r0 = r;
-% initialConditions.v0 = v;
-
+% Collect into one signal
 initialConditions.all   = [ initialConditions.r0; initialConditions.v0;
                             initialConditions.q0; initialConditions.w0 ];
 simParams.initialConditions = initialConditions;
