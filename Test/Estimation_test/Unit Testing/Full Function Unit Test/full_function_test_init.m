fb19ad17d72eaaa741c3cbaa19139ff543c9a279
kashton6 <39278155+kashton6@users.noreply.github.com>
2020-02-02 21:44:28 -0800

    got all intputs/outputs synced  and right dimensions/conversions

--- Test/Estimation_test/Unit Testing/Full Function Unit Test/full_function_test_init.m
+++ Test/Estimation_test/Unit Testing/Full Function Unit Test/full_function_test_init.m
@@ -1,4 +1,9 @@
 simParams.sample_time_s = .1;
+tspan = 0:simParams.sample_time_s:10;
+m = length(tspan);
 set_param(bdroot,'ShowPortDataTypes','on')
 set_param(bdroot,'ShowLineDimensions','on')
+valid1 = [tspan',ones(m,1)]; % gives health of sun sensor 1 == valid, 0 == invalid data
+  valid2 = [tspan',ones(m,1)]; % gives health of mag sensor 1 == valid, 0 == invalid data
+    valid3 = [tspan',ones(m,1)];
 % simParams.initialConditions.w0 = zeros(3,1);
