feac62078ec7ab0205e1490c38f81578d290cfbf
kashton6 <39278155+kashton6@users.noreply.github.com>
2020-02-27 20:06:10 -0800

    rearrranging library for readability

--- Lib/FSW/attitudeEstimation/EKF_kylle/init_MEKF.m
+++ Lib/FSW/attitudeEstimation/EKF_kylle/init_MEKF.m
@@ -1,55 +1,40 @@
-% Loads the parameters of the estimation estimator using predefined fsw_params.
-%   Any block added should be initialized and defined in here.
+%%%%% THIS SCRIPT SETS UP PARAMETERS AND INITIAL CONDITIONS FOR the simulink estimator
+                                     
+estimation = struct;
 
-% UW HuskySat-1, ADCS Team
-% Last Edited: T. Reynolds 8.8.17
-% ----------------------------------------------------------------------- %
+%%%% Initial Conditions %%%%%
+   
+estimation.ic.Beta_init = [0;0;0];%Initialize gyro Bias
+estimation.ic.quat_est_init = [1;0;0;0]; 
+% estimation.ic.quat_est_init = simParams.initialConditions.q0;
 
-% Library sample time
-estimation.sample_time_s = 1/10;
+estimation.ic.w_init = [0.00;0.00;0.00];
 
-% Initial conditions
-estimation.ic.quat_est_init = [1 0 0 0]';
-estimation.ic.rate_est_init = [0 0 0]';
-estimation.ic.bias_est_init = [0 0 0]';
-% Initial covariance from Crassidis Ex. 7.1 pp. 460.
-P_0_a = 3.0462e-6;  % attitude
-P_0_b = 9.4018e-13; % bias
-estimation.ic.error_cov = blkdiag(P_0_a*eye(3),P_0_b*eye(3));
-    % rate transition blocks
-    estimation.ic.rt_valid_gyro    = 0;
-    estimation.ic.rt_valid_mag     = 0;
-    estimation.ic.rt_valid_sun     = 0;
-    estimation.ic.rt_mt_power_ok   = 0;
-    estimation.ic.rt_sc_in_sun     = 0;
-    estimation.ic.rt_w_body_radps  = zeros(3,1);
-    estimation.ic.rt_mag_body      = zeros(3,1);
-    estimation.ic.rt_mag_eci_est   = zeros(3,1);
-    estimation.ic.rt_sun_body      = zeros(3,1);
-    estimation.ic.rt_sun_eci_est   = zeros(3,1);
-    estimation.ic.rt_meas_cov      = zeros(6);
-
-% Upper bound on convergence time
-estimation.converge_time_s   = 60;
-
-% Constant matrices
-estimation.G   = blkdiag(-eye(3),eye(3));
-% This dt MUST be the rate at which information in the KF is updated!
-estimation.dt  = estimation.sample_time_s;
-
+%Create covariance matrix for estimate
+ P_0_a = (0.1*pi/180)^2;  % attitude
+P_0_b = (0.2*pi/180/3600); % bias
+estimation.ic.P_init = blkdiag(P_0_a*eye(3),P_0_b*eye(3));
 
 % Process and measurement covariances
-sig_v   = sqrt(10)*1e-7;            % angle random walk
-sig_u   = sqrt(10)*1e-10;           % rate random walk
-dt      = estimation.sample_time_s;
-mag_err = norm([1.637696e-7;9.22938e-8;1.343961e-7]);
-sun_err = 0.05;
 
-estimation.proc_cov = [(sig_v^2*dt + 1/3*sig_u^2*dt^3)*eye(3)    -(1/2*sig_u^2*dt^2)*eye(3);
-                   -(1/2*sig_u^2*dt^2)*eye(3)              (sig_u^2*dt)*eye(3)];
-estimation.meas_cov = diag([mag_err*ones(1,3), sun_err*ones(1,3)]);
+sig_v = sqrt(10)*1e-7;     % angle random walk
+sig_u = sqrt(10)*1e-10;    % rate random walk
+
+sun_sensor_var = 0.5/(sqrt(2)*3.0); % sun sensor measurement covariance (radians)
+mag_var =  10^-6*[0.403053;0.240996;0.173209]; % magnetometer covariance (micro tesla)
+
+
+%%Time step that the MEKF is ran at
+estimation.dt = fswParams.sample_time_s; 
+dt = estimation.dt;
+
+estimation.Q_k = [(sig_v^2*dt + 1/3*sig_u^2*dt^3)*eye(3)    -(1/2*sig_u^2*dt^2)*eye(3);
+                   -(1/2*sig_u^2*dt^2)*eye(3)              (sig_u^2*dt)*eye(3)]; %create dynamic nnoise measurement matrix
+%Constant MAtrices
+estimation.gamma = blkdiag(-eye(3),eye(3));
+estimation.Qg = estimation.gamma*estimation.Q_k*estimation.gamma';
+estimation.R = [sun_sensor_var^2*eye(3),zeros(3,3);zeros(3,3),((mag_var.^2)).*eye(3)]; %create measurment error cov matrix
 
 fswParams.estimation = estimation;
-
-clear estimation;
-clear P_0_a P_0_b sig_v sig_u dt mag_err sun_err; 
+clear estimation
+clear sig_v sig_u sun_sensor_var mag_var zero R Beta P
