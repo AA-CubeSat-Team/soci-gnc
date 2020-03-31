739fd0dcb837fd16e8e96fc17692a33e49729f8b
devantormey <devan.tormey@uscrpl.com>
2020-03-31 10:27:11 -0700

    something was wrong with sunvector autocode  stuff so i tracked it

---
+++ Test/Autocoding_tests/Sunvector_unit_test/html/traceInfo_flag.js
@@ -0,0 +1,42 @@
+function TraceInfoFlag() {
+    this.traceFlag = new Array();
+    this.traceFlag["sunVector_test.c:422c8"]=1;
+    this.traceFlag["sunVector_test.c:423c13"]=1;
+    this.traceFlag["sunVector_test.c:424c8"]=1;
+    this.traceFlag["sunVector_test.c:425c24"]=1;
+    this.traceFlag["sunVector_test.c:425c32"]=1;
+    this.traceFlag["sunVector_test.c:425c59"]=1;
+    this.traceFlag["sunVector_test.c:425c73"]=1;
+    this.traceFlag["sunVector_test.c:428c8"]=1;
+    this.traceFlag["sunVector_test.c:429c7"]=1;
+    this.traceFlag["sunVector_test.c:430c8"]=1;
+    this.traceFlag["sunVector_test.c:432c8"]=1;
+    this.traceFlag["sunVector_test.c:434c8"]=1;
+    this.traceFlag["sunVector_test.c:436c8"]=1;
+    this.traceFlag["sunVector_test.c:437c8"]=1;
+    this.traceFlag["sunVector_test.c:438c8"]=1;
+}
+top.TraceInfoFlag.instance = new TraceInfoFlag();
+function TraceInfoLineFlag() {
+    this.lineTraceFlag = new Array();
+    this.lineTraceFlag["sunVector_test.c:420"]=1;
+    this.lineTraceFlag["sunVector_test.c:421"]=1;
+    this.lineTraceFlag["sunVector_test.c:422"]=1;
+    this.lineTraceFlag["sunVector_test.c:423"]=1;
+    this.lineTraceFlag["sunVector_test.c:424"]=1;
+    this.lineTraceFlag["sunVector_test.c:425"]=1;
+    this.lineTraceFlag["sunVector_test.c:426"]=1;
+    this.lineTraceFlag["sunVector_test.c:427"]=1;
+    this.lineTraceFlag["sunVector_test.c:428"]=1;
+    this.lineTraceFlag["sunVector_test.c:429"]=1;
+    this.lineTraceFlag["sunVector_test.c:430"]=1;
+    this.lineTraceFlag["sunVector_test.c:431"]=1;
+    this.lineTraceFlag["sunVector_test.c:432"]=1;
+    this.lineTraceFlag["sunVector_test.c:433"]=1;
+    this.lineTraceFlag["sunVector_test.c:434"]=1;
+    this.lineTraceFlag["sunVector_test.c:435"]=1;
+    this.lineTraceFlag["sunVector_test.c:436"]=1;
+    this.lineTraceFlag["sunVector_test.c:437"]=1;
+    this.lineTraceFlag["sunVector_test.c:438"]=1;
+}
+top.TraceInfoLineFlag.instance = new TraceInfoLineFlag();
