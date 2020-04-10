b7f1244bc20c9650df81ca06a9f8afbe6c568428
colemorgan32 <39118696+colemorgan32@users.noreply.github.com>
2020-04-09 17:57:09 -0700

    Fixed rate transition in allocation. changed desat condition to be based on inf norm AND 2 norm of wheel rpms.

--- Lib/FSW/FSW_init.m
+++ Lib/FSW/FSW_init.m
@@ -8,11 +8,13 @@
 mode_select       = struct;
 target_generation = struct;
 
-mode_select.sc_mode_ic              = int8(1.0);
-mode_select.body_rate_threshold_max = 9*pi/180;  %rad/s
-mode_select.body_rate_threshold_min = .5*pi/180; %rad/s
-mode_select.RWA_RPM_threshold_max   = 4500; %RPM
-mode_select.RWA_RPM_threshold_min   = 1100; %RPM
+mode_select.sc_mode_ic                    = int8(1.0);
+mode_select.body_rate_threshold_max       = 9*pi/180;  %rad/s
+mode_select.body_rate_threshold_min       = .5*pi/180; %rad/s
+mode_select.RWA_RPM_threshold_max_inf     = 5000;   %RPM. limits max indv rpm
+mode_select.RWA_RPM_threshold_min_inf     = 1100;   %RPM.
+mode_select.RWA_RPM_threshold_max_2norm   = 2*3000; %RPM. limits total power
+mode_select.RWA_RPM_threshold_min_2norm   = 2*1100; %RPM. 
 
 % Define our orbit
 % YMDHMS  = [ 2020; 1; 1; 0; 0; 0 ];
