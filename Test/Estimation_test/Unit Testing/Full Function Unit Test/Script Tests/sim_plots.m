4b39ecd0683d30635c42979a14a2e4715fb43696
kashton6 <39278155+kashton6@users.noreply.github.com>
2020-06-05 15:05:43 -0700

    Took off time propagation bypass of cavariance matrix in mekf_lib

--- Test/Estimation_test/Unit Testing/Full Function Unit Test/Script Tests/sim_plots.m
+++ Test/Estimation_test/Unit Testing/Full Function Unit Test/Script Tests/sim_plots.m
@@ -5,12 +5,13 @@
 
 clc
 close all
-tfinal = 2000;
+tfinal = 20000;
 t = 0:fswParams.sample_time_s:tfinal;
 qest = my_qest.Data';
 qtrue = q_true.Data';
-bias = reshape(bias_out.signals.values,3,length(t))';
-omega_est1 = reshape(omega_est.signals.values,3,length(t))';
+% bias = reshape(bias_out.signals.values,3,length(t))';
+bias = bias_out.Data';
+omega_est1 = omega_est.Data'; %reshape(omega_est.signals.values,3,length(t))';
 for i =1:length(t)
  qmix1 = [qest(4,i);qest(1,i);qest(2,i);qest(3,i)]';
                 qmix = ([qest(4,i);-qest(1,i);-qest(2,i);-qest(3,i)]');
@@ -53,12 +54,13 @@ grid on;
 subplot(4,1,j)
 hold on
 if j ==1
-   h1 = plot(t,q_err.Data(:,j),'color',Purple,'Linewidth',1.25)
+%    h1 = plot(t,q_err.Data(:,j),'color',Purple,'Linewidth',1.25)
+   h1 = plot(t,quat_theta_error_deg.Data(:,j),'color',Purple,'Linewidth',1.25)
     legend(h1,['AKE error']);
     title('Attitude Knowledge Error')
     ylabel('Error (deg)')
  xlabel('Time (hours)')
-%  xlim([5500,tfinal])
+ xlim([100,tfinal])
       xt = round([0:(tfinal/3600)/11:tfinal/3600],2);
     xt(end) = tfinal/3600;
 set(gca,'XTick',0:tfinal/11:tfinal)
@@ -71,7 +73,7 @@ legend(h,[qleg{j},' error']);
 title([qleg{j},' error and 3 \sigma Bounds']) 
 ylabel('Error (deg)')
  xlabel('Time (hours)')
- xlim([5500,tfinal])
+ xlim([20,tfinal])
       xt = round([0:(tfinal/3600)/11:tfinal/3600],2);
     xt(end) = tfinal/3600;
 set(gca,'XTick',0:tfinal/11:tfinal)
@@ -81,15 +83,31 @@ hold off
 end
 end
 
+omeg_err = omega_est1-wtrue.Data';
+for k = 1:length(t)
+    om_nerr(k) = norm(omeg_err(:,k),2);
+end
 ax1 = {'\omega_x','\omega_y','\omega_z'};
  figure;
-for j = 1:3
+for j = 1:4
 grid on;
-subplot(3,1,j)
+subplot(4,1,j)
 hold on
-plot(t,omega_est1(:,j),'color',Purple,'Linewidth',1.1)
-% legend(h,['q_',num2str(j),' error']);
+if j<4
+ plot(t,omega_est1(j,:),'color',Purple,'Linewidth',1.1)
+ plot(t,wtrue.Data(:,j)','color',Gold,'Linewidth',1.1)
+legend(['q_',num2str(j),' error'],'\omega true');
 title(['Estimated ',ax1{j}]) 
+ylabel('Value (deg/s)')
+ xlabel('Time (hours)')
+ xt = round([0:(tfinal/3600)/11:tfinal/3600],2);
+    xt(end) = tfinal/3600;
+set(gca,'XTick',0:tfinal/11:tfinal)
+set(gca,'XTickLabel',xt)
+else 
+    g1 = plot(t,om_nerr(j),'color',Purple,'Linewidth',1.1)
+legend(g1,['q_',num2str(j),' error']);
+% title(['Estimated ',ax1{j}]) 
 ylabel('Value (deg/s)')
  xlabel('Time (hours)')
  xt = round([0:(tfinal/3600)/11:tfinal/3600],2);
@@ -99,6 +117,7 @@ set(gca,'XTickLabel',xt)
 grid on;
 hold off
 end
+end
 
 
 ax = {'\beta_x','\beta_y','\beta_z'};
@@ -107,7 +126,7 @@ for j = 1:3
 grid on;
 subplot(3,1,j)
 hold on
-plot(t,bias(:,j),'color',Purple,'Linewidth',1.1)
+plot(t,bias(j,:),'color',Purple,'Linewidth',1.1)
 % legend(h,['q_',num2str(j),' error']);
 title([ax{j},' Value']) 
 ylabel('Bias (deg/s)')
