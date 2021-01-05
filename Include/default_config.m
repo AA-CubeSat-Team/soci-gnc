240846df767b645547e4b65fa177e1e8f73f6251
Taylor Patrick Reynolds <tpr6@uw.edu>
2021-01-05 14:17:03 -0500

    removed power signals from actuator measurements going into flight software.

--- Include/default_config.m
+++ Include/default_config.m
@@ -22,10 +22,10 @@ simParams.opts.sensor_noise = 0;
 simParams.opts.actuator_model = 0;
 
 % FSW Parameters 
-fswParams.sample_time_s = 0.1; % sample at 10Hz
+fswParams.sample_time_s = 1/10;     % sample at 10Hz
 
 % Sim Parameters s
-simParams.sample_time_s = 1/200; % sample at 200Hz
+simParams.sample_time_s = 1/200;    % sample at 200Hz
 
 % Set initial CDH commands
 cdh = Simulink.Mask.get('Main_Sim/CDH_command_lib');
