293008e99e762bed2357bcf66923bd07550e6134
Nick Melville <nm2020@uw.edu>
2021-04-24 15:39:03 -0700

    Added alloc_null

--- Lib/FSW/allocation/allocator_init.m
+++ Lib/FSW/allocation/allocator_init.m
@@ -22,6 +22,7 @@ RPM2RPS     = fswParams.constants.convert.RPM2RPS;
 targ_rpm    = [ 1000; -1000; 1000; -1000 ];
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 nullvec     = [1;-1;1;-1];
 =======
 rwa_nullvec = [1;-1;1;-1];
@@ -29,6 +30,9 @@ rwa_nullvec = [1;-1;1;-1];
 =======
 nullvec     = [1;-1;1;-1];
 >>>>>>> b3933fd (Hopefully this will allow me to make a pull request.)
+=======
+rwa_nullvec = [1;-1;1;-1];
+>>>>>>> 40d7965 (Added alloc_null)
 
 alloc = struct;
 
@@ -40,6 +44,7 @@ alloc.max_RPM             = rwa_sim.max_RPM;
 alloc.num_facet           = uint8(6);
 alloc.h_targ_wheel_Nms    = RPM2RPS .* ( Jw * targ_rpm );
 alloc.feedback_gain       = 0.01;
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -55,6 +60,9 @@ alloc.rwa_nullvec         = rwa_nullvec;
 alloc.nullvec             = nullvec;
 >>>>>>> b3933fd (Hopefully this will allow me to make a pull request.)
 
+=======
+alloc.rwa_nullvec         = rwa_nullvec
+>>>>>>> 40d7965 (Added alloc_null)
 
 %%%%
 % THIS QUANTITY IS HARD CODED IN AND PARTICULAR TO THE 23 DEGREE
