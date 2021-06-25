7c95a8a7471a981ca0d2edcc0ee0b2fcd02db7e3
Nick Melville <nm2020@uw.edu>
2021-06-25 12:56:02 -0700

    Updates to Tests

--- Test/FSW/TestPlan/ASM_Test.m
+++ Test/FSW/TestPlan/ASM_Test.m
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function asm_results = ASM_Test(testParams, fswParams)
 
 %% Initialize the parameters
@@ -78,16 +79,42 @@ final_rate = norm(norm(w_body_radps.Data(end,:)));
 %% Plots the data
 figure(1)
 subplot(3,1,1)
+=======
+function ASM_Test(testParams)
+
+init_params; 
+
+% READ THIS: Run Sim_init if you haven't already.
+
+% Sim_init
+
+% Sets the initial body rotation rate to two times the threshold for
+% attitude stabilization mode. 
+simParams.initialConditions.w_body_radps = 2*[1;1;1]*fswParams.mode_select.body_rate_threshold_max;
+
+a = sim('Main_Sim', 'StartTime','0','StopTime','30');
+gnc_mode = a.get('gnc_mode');
+sc_body_rates_radps = a.get('sc_body_rates_radps');
+
+figure(1)
+subplot(2,1,1)
+>>>>>>> 96c8442 (Updates to Tests)
 plot(gnc_mode.Time,gnc_mode.Data,'LineWidth',3')
 title('SOC-I Attitude Stabilization Mode')
 ylabel('GNC Mode')
 
+<<<<<<< HEAD
 subplot(3,1,2)
 plot(w_body_radps.Time,w_body_radps.Data,'LineWidth',2')
+=======
+subplot(2,1,2)
+plot(sc_body_rates_radps.Time,sc_body_rates_radps.Data,'LineWidth',2')
+>>>>>>> 96c8442 (Updates to Tests)
 xlabel('Time (s)')
 ylabel('Body Rates (rad/s)')
 legend('x','y','z')
 
+<<<<<<< HEAD
 subplot(3,1,3)
 plot(rwa_cmd_rpm.Time,rwa_cmd_rpm.Data,'LineWidth',2')
 xlabel('Time (s)')
@@ -158,6 +185,69 @@ asm_results.timely_detumble = timely_detumble;
 asm_results.nomore_tumble = nomore_tumble; 
 asm_results.rate_crit = rate_crit;
 %asm_results.new_gnc = new_gnc;
+=======
+if master_test ~= 1
+    fileID = fopen('Test_Plan_Results','w');
+end
+
+fprintf(testParams.fileID,'ASM (Detumble) Test\n\n');
+
+% The satellite has 30 seconds to detumble. There is no time requirements,
+% but this should be easily met. 
+dt = 30/simParams.sample_time_s;
+
+w_final = sc_body_rates_radps.Data(dt,:);
+
+w_mag_final = sqrt(w_final(1)^2 + w_final(2)^2 + w_final(3)^2);
+
+fprintf(fileID,'After 30 seconds, does the satellite have a low rotation rate?\n');
+
+if (w1(end)<=fswParams.mode_select.body_rate_threshold_max)
+    
+    fprintf(fileID,'Pass\n');
+    formatSpec = 'The rotation rate after 30 seconds of detumbling is %1.4f rad/s\n\n';
+    fprintf(fileID,formatSpec,w_mag_final)
+    
+else
+    
+    fprintf(fileID,'Fail\n');   
+    formatSpec = 'The rotation rate after 30 seconds of detumbling is %1.4f rad/s\n\n';
+    fprintf(fileID,formatSpec,w_mag_final)
+    
+end
+
+timestep_left_ASM = find(gnc_mode.Data == 5 | gnc_mode.Data == 5 ,  1, 'first')
+
+w_crit = sc_body_rates_radps.Data(timestep_left_ASM,:);
+
+w_crit_mag = sqrt(w_crit(1)^2 + w_crit(2)^2 + w_crit(3)^2);
+
+fprintf(fileID,'After the satellite switches out of detumble mode, does it actually have a low rotation rate?\n');
+
+if (w_crit_mag <=fswParams.mode_select.body_rate_threshold_max)
+    
+    fprintf(fileID,'Pass\n');
+    formatSpec = 'The rotation rate after the satellite switches out of detumble mode is %1.4f rad/s\n';
+    fprintf(fileID,formatSpec,w_crit_mag)
+    formatSpec = 'The new GNC mode is %1.1f \n\n';
+    fprintf(fileID,formatSpec,gnc_mode.Data(timestep_left_ASM))
+else
+    
+    
+
+    fprintf(fileID,'Fail\n');
+    formatSpec = 'The rotation rate after the satellite switches out of detumble mode is %1.4f rad/s\n';
+    fprintf(fileID,formatSpec,w_crit_mag)
+    formatSpec = 'The new GNC mode is %1.1f \n\n';
+    fprintf(fileID,formatSpec,gnc_mode.Data(timestep_left_ASM))
+    
+end
+
+
+if master_test ~= 1
+    fclose(fileID);
+end
+>>>>>>> 96c8442 (Updates to Tests)
 
 end
     
--- Test/FSW/TestPlan/Imaging_Test.m
+++ Test/FSW/TestPlan/Imaging_Test.m
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function asm_results = Imaging_Test(testParams, fswParams)
 
 %% Initialize the parameters
@@ -274,4 +275,62 @@ asm_results.nomore_tumble = nomore_tumble;
 asm_results.rate_crit = rate_crit;
 %asm_results.new_gnc = new_gnc;
 
+=======
+% Imaging Test
+
+init_params; 
+
+% READ THIS: Run Sim_init if you haven't already. For this test to work, 
+% you must change the target lat, long, and alt to 60,120,0. This ensures 
+% satellite flies over the ground target in a timely manner. You must also 
+% enable imaging. 
+
+% Sim_init
+
+fprintf(fileID,'Imaging Test\n\n');
+
+a = sim('Main_Sim', 'StartTime','0','StopTime','500');
+gnc_mode = a.get('gnc_mode');
+ace_err = a.get('ace_err');
+
+figure(1)
+subplot(2,1,1)
+plot(gnc_mode.Time,gnc_mode.Data,'LineWidth',3')
+title('SOC-I Imaging Mode')
+ylabel('GNC Mode')
+
+subplot(2,1,2)
+plot(ace_err.Time,ace_err.Data,'LineWidth',2')
+xlabel('Time (s)')
+ylabel('Attitude Error (degrees)')
+
+if master_test == 0
+    fileID = fopen('Test_Plan_Results','w');
+end
+
+% The satellite has 30 seconds to orient its camera. There is no time requirements,
+% but this should be easily met. 
+dt = 30/simParams.sample_time_s;
+
+timestep_into_imaging = find(gnc_mode.Data == 7,  1, 'first')
+
+ace_imaging = ace_err.Data(timestep_into_imaging + dt)
+
+if (ace_imaging<=1)
+    
+    fprintf(fileID,'Pass\n');
+    formatSpec = 'The attitude control error after 30 seconds in imaging mode is %1.4f degrees\n\n';
+    fprintf(fileID,formatSpec,ace_imaging)
+    
+else
+    
+    fprintf(fileID,'Fail\n');  
+    formatSpec = 'The attitude control error after 30 seconds in imaging mode is %1.4f degrees\n\n';
+    fprintf(fileID,formatSpec,ace_imaging)
+    
+end
+
+if (master_test == 0)
+    fclose(fileID);
+>>>>>>> 96c8442 (Updates to Tests)
 end
