94a60c754e357ce1314adbe1b5b0247d84f3b914
Nick Melville <nm2020@uw.edu>
2021-07-13 21:56:08 -0700

    Added gs_above_targ to the fsw_telemetry bus so that I can use it for testing. Put default_config back in the right place. Added another output to FSW_Lib so that I can use it as part of testing. Fixed an error in the eci to ecef conversion which was causing the Earth to rotate in the wrong direction. Fixed a sign error in groundpass_predict_lib. Fixed an obvious bug in mode_select_lib which made img mode have the wrong attitude targets. Added a bunch of new code to Imaging_Test and Master_Test and testParams_init.

--- Test/FSW/TestPlan/testParams_init.m
+++ Test/FSW/TestPlan/testParams_init.m
@@ -7,6 +7,11 @@
 testParams = struct;
 
 testParams.save_figs = true;
+testParams.fileID = fopen('Test_Results', 'w');
 
 % Time given for ACE to go to zero after a gnc mode change (seconds).
 testParams.maneuver_time = 45;
+
+% Time given for the attitude determination system to determine the
+% attitude
+testParams.attitude_acq_time = 5;
